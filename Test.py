46897916b8554303888df0f3d5e50ebcbe51a46e
HeetVasani1 <82655384+HeetVasani1@users.noreply.github.com>
2022-11-25 12:24:07 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ import unittest
 
 from Divsion import division
 
-class TestSum(unittest.TestCase):
+class TestSum1(unittest.TestCase):
     def test_list_int(self):
         """
         Test case to division two numbers
@@ -12,7 +12,52 @@ class TestSum(unittest.TestCase):
         a = 30
         b = 15
         result = division(a,b)
+        self.assertEqual(result, 2)
+        
+
+class TestSum2(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to division two numbers
+        """
+        a = 40
+        b = 8
+        result = division(a,b)
+        self.assertEqual(result, 5)
+        
+  
+
+class TestSum3(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to division two numbers
+        """
+        a = 300
+        b = 15
+        result = division(a,b)
         self.assertEqual(result, 20)
+        
+
+class TestSum4(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to division two numbers
+        """
+        a = 35
+        b = 7
+        result = division(a,b)
+        self.assertEqual(result, 20)
+        
+        
+class TestSum5(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to division two numbers
+        """
+        a = 36
+        b = 15
+        result = division(a,b)
+        self.assertEqual(result, 25)
 
 if __name__ == '__main__':
     unittest.main()
